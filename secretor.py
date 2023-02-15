4af4735cb2aa4ae43cc9af2cc29ea785c1e69821
iseya <0610iseya@gmail.com>
2023-02-15 15:49:13 +0900

    Add get secret method

---
+++ secretor.py
@@ -0,0 +1,58 @@
+from google.cloud import secretmanager, exceptions as exc, logging
+from retry import retry
+import os
+
+
+@retry(Exception, tries=3, delay=2)
+def get_secret(project_id: str, secret_name: str, version='latest') -> any:
+    """Get secret from SecretManager.
+
+    Args:
+        project_id (str): GCP ProjectId.
+        secret_name (str): SecretID set in Secret Manager.
+        version (str): Secret Version. Default value is latest.
+
+    Returns:
+        any: The value associated with secret_id.
+
+    Raises:
+        NotFound: secret_id does not exist or is incorrect.
+        Exception: Some Error Occurred.
+    """
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/{version}"
+    client = secretmanager.SecretManagerServiceClient()
+
+    try:
+        res = client.access_secret_version(request={"name": name}).payload.data.decode("UTF-8")
+        return res
+    except exc.NotFound:
+        raise _send_log("Failed to get secret.", f"Secret {secret_name} was not found.", "ERROR")
+    except Exception as err:
+        _send_log("Some Error Occurred so Retrying...", str(err), "WARNING")
+        raise _send_log("Some Error Occurred.", str(err), "ERROR")
+
+
+def _send_log(overview: str, details: str, severity: str):
+    """Send log to CloudLogging.
+
+    Args:
+        overview (str): Describe the log summary.
+        details (str): Describe the log details.
+        severity (str): Select NOTICE/WARNING/ERROR.
+    """
+    client = logging.Client()
+    client.setup_logging()
+    logger = client.logger(os.getenv("FUNCTION_TARGET", default="local"))
+
+    logger.log_struct(
+        {
+            "severity": severity,
+            "message": overview,
+            "details": details,
+        },
+    )
+
+
+if __name__ == '__main__':
+    a = get_secret("nari-training", "SERVICE_ACCOUNT_FORCED_LOGOUT")
+    print(a)
